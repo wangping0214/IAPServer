51072966cbc1cb64ca9b1236cd64551bb01df0be
Alanmars <ping.wang@zentertain.net>
2014-06-11 01:25:10 +0800

    Change the produce and consume type of rest service from json to xml.

--- src/com/rainbow/iap/rest/ChinaUnicomIAPService.java
+++ src/com/rainbow/iap/rest/ChinaUnicomIAPService.java
@@ -1,14 +1,31 @@
 package com.rainbow.iap.rest;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import com.rainbow.iap.dao.impl.UnicomIAPDAOImpl;
 import com.rainbow.iap.entity.UnicomIAPInfo;
@@ -17,117 +34,140 @@ import com.rainbow.iap.util.IAPDateFormatter;
 @Path("/ChinaUnicomIAPService")
 public class ChinaUnicomIAPService
 {
-	private static final String VALIDATE_ORDER_ID 	= "validateorderid";
-	private static final String NOTIFY_RESULT		= "notifyresult";
-	
 	@POST
-    @Path("/request/post/{serviceId}")
-    @Produces(MediaType.APPLICATION_JSON)
-	@Consumes({MediaType.APPLICATION_JSON})
-	public JSONObject postValidateOrderId(@PathParam("serviceId") String serviceId, 
-			JSONObject request)
+    @Path("/request/post")
+    @Produces(MediaType.APPLICATION_XML)
+	@Consumes({MediaType.APPLICATION_XML})
+	public String postValidateOrderId(String xmlContent)
 	{
-		if (VALIDATE_ORDER_ID.equals(serviceId))
+		System.out.println("recv: " + xmlContent);
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+		try
 		{
-			try
+			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			Document doc = docBuilder.parse(new InputSource(new StringReader(xmlContent)));
+			Element rootElem = doc.getDocumentElement();
+			
+			String orderId = null;
+			String orderTime = null;
+			String cpId = null;
+			String consumeCode = null;
+			String hRet = null;
+			String status = null;
+			String signMsg = null;
+			NodeList childNodes = rootElem.getChildNodes();
+			for (int i = 0; i < childNodes.getLength(); ++ i)
 			{
-				return validateOrderId(request);
-			} catch (JSONException e)
-			{
-				e.printStackTrace();
+				Node childNode = childNodes.item(i);
+				if (childNode instanceof Element)
+				{
+					Element childElem = (Element) childNode;
+					if (childElem.getTagName().equals("orderid"))
+					{
+						orderId = childElem.getTextContent();
+					}
+					else if (childElem.getTagName().equals("ordertime"))
+					{
+						orderTime = childElem.getTextContent();
+					}
+					else if (childElem.getTagName().equals("cpid"))
+					{
+						cpId = childElem.getTextContent();
+					}
+					else if (childElem.getTagName().equals("consumeCode"))
+					{
+						consumeCode = childElem.getTextContent();
+					}
+					else if (childElem.getTagName().equals("hRet"))
+					{
+						hRet = childElem.getTextContent();
+					}
+					else if (childElem.getTagName().equals("status"))
+					{
+						status = childElem.getTextContent();
+					}
+					else if (childElem.getTagName().equals("signMsg"))
+					{
+						signMsg = childElem.getTextContent();
+					}
+				}
 			}
-		}
-		else if (NOTIFY_RESULT.equals(serviceId))
-		{
-			try
+			
+			Document resultDoc = docBuilder.newDocument();
+			if (consumeCode == null)	//validate ordier id
 			{
-				return notifyResult(request);
-			} catch (JSONException e)
-			{
-				e.printStackTrace();
+				Element resElem = resultDoc.createElement("checkOrderIdRsp");
+				if (orderId != null && signMsg != null)
+				{
+					//validate orderId and signMsg
+					resElem.setTextContent("0");
+				}
+				else
+				{
+					resElem.setTextContent("1");
+				}
+				resultDoc.appendChild(resElem);
 			}
-		}
-		return new JSONObject();
-	}
-	
-	private JSONObject validateOrderId(JSONObject request) throws JSONException
-	{
-		String orderId = null;
-		String signMsg = null;
-		if (request.has("orderid"))
-		{
-			orderId = request.getString("orderid");
-		}
-		if (request.has("signMsg"))
-		{
-			signMsg = request.getString("signMsg");
-		}
-		int result = 1;
-		if (orderId != null && signMsg != null)
-		{
-			result = 0;
-		}
-		JSONObject jsonResult = new JSONObject();
-		jsonResult.put("checkOrderIdRsp", result);
-		return jsonResult;
-	}
-	
-	private JSONObject notifyResult(JSONObject request) throws JSONException
-	{
-		String orderId = null;
-		String orderTime = null;
-		String cpId = null;
-		String consumeCode = null;
-		int hRet = 1;
-		int status = 0;
-		String signMsg = null;
-		if (request.has("orderid"))
-		{
-			orderId = request.getString("orderid");
-		}
-		if (request.has("ordertime"))
-		{
-			orderTime = request.getString("ordertime");
-		}
-		if (request.has("cpid"))
-		{
-			cpId = request.getString("cpid");
-		}
-		if (request.has("consumeCode"))
-		{
-			consumeCode = request.getString("consumeCode");
-		}
-		if (request.has("hRet"))
-		{
-			hRet = request.getInt("hRet");
-		}
-		if (request.has("status"))
-		{
-			status = request.getInt("status");
-		}
-		if (request.has("signMsg"))
-		{
-			signMsg = request.getString("signMsg");
-		}
-		if (orderId != null && signMsg != null)
-		{
-			//handle the result
-			//persist them
-			UnicomIAPInfo unicomIapInfo = UnicomIAPDAOImpl.getInstance().getByOrderId(orderId);
-			if (unicomIapInfo == null)
+			else						//notify result
 			{
-				unicomIapInfo = new UnicomIAPInfo();
-				unicomIapInfo.setOrderId(orderId);
-				unicomIapInfo.setOrderTime(IAPDateFormatter.strToTime(orderTime, "yyyyMMddHHmmss"));
-				unicomIapInfo.setCpId(cpId);
-				unicomIapInfo.setConsumeCode(consumeCode);
-				unicomIapInfo.setOrderResult(hRet);
-				unicomIapInfo.setOrderStatus(status);
-				UnicomIAPDAOImpl.getInstance().save(unicomIapInfo);
+				Element resElem = resultDoc.createElement("callbackRsp");
+				if (orderId != null && signMsg != null)
+				{
+					//handle the result
+					//persist them
+					UnicomIAPInfo unicomIapInfo = UnicomIAPDAOImpl.getInstance().getByOrderId(orderId);
+					if (unicomIapInfo == null)
+					{
+						unicomIapInfo = new UnicomIAPInfo();
+						unicomIapInfo.setOrderId(orderId);
+						if (orderTime != null)
+						{
+							unicomIapInfo.setOrderTime(IAPDateFormatter.strToTime(orderTime, "yyyyMMddHHmmss"));
+						}
+						unicomIapInfo.setCpId(cpId);
+						unicomIapInfo.setConsumeCode(consumeCode);
+						if (hRet != null)
+						{
+							unicomIapInfo.setOrderResult(Integer.parseInt(hRet));
+						}
+						if (status != null)
+						{
+							unicomIapInfo.setOrderStatus(Integer.parseInt(status));
+						}
+						UnicomIAPDAOImpl.getInstance().save(unicomIapInfo);
+					}
+				}
+				resElem.setTextContent("0");
+				resultDoc.appendChild(resElem);
+				
 			}
+			StringWriter strWriter = new StringWriter();
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty("indent", "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+			transformer.transform(new DOMSource(resultDoc), new StreamResult(strWriter));
+			System.out.println("send: " + strWriter.toString());
+			return strWriter.toString();
+		} catch (ParserConfigurationException e)
+		{
+			e.printStackTrace();
+		} catch (SAXException e)
+		{
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e)
+		{
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e)
+		{
+			e.printStackTrace();
+		} catch (TransformerException e)
+		{
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
 		}
-		JSONObject jsonResult = new JSONObject();
-		jsonResult.put("callbackRsp", 0);
-		return jsonResult;
+		
+		return "";
 	}
 }
