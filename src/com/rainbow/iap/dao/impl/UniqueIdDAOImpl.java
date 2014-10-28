64b034e515b0b496351f5021c5dc2f5ab961c87f
Alanmars <wangping0214@126.com>
2014-10-28 23:51:41 +0800

    1.Persist unique id seed of cp_app_id at fixed rate. 2.Only fetch seed of order id.

--- src/com/rainbow/iap/dao/impl/UniqueIdDAOImpl.java
+++ src/com/rainbow/iap/dao/impl/UniqueIdDAOImpl.java
@@ -9,13 +9,30 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.rainbow.iap.dao.UniqueIdDAO;
+import com.rainbow.iap.util.Scheduler;
 
 public class UniqueIdDAOImpl implements UniqueIdDAO
 {
+	private static final Log logger = LogFactory.getLog(UniqueIdDAOImpl.class);
+	private static final int CP_APP_ID_LENGHT = 12;
+	private static final long PERSIST_INTERVAL = 600000;	//persist every 10 mins
+	private class PersistTask implements Runnable
+	{
+		@Override
+		public void run()
+		{
+			update();
+		}
+	}
 	private static UniqueIdDAO _instance = new UniqueIdDAOImpl();
 	
 	public static UniqueIdDAO getInstance()
@@ -26,6 +43,7 @@ public class UniqueIdDAOImpl implements UniqueIdDAO
 	private Set<String>				_newUniqueIdSet;
 	private Map<String, AtomicLong>	_uniqueIdMap;
 	private ReentrantLock			_lock;
+	private ScheduledFuture<?>		_future;
 	
 	private UniqueIdDAOImpl()
 	{
@@ -48,13 +66,19 @@ public class UniqueIdDAOImpl implements UniqueIdDAO
 			{
 				String uniqueIdName = rs.getString("unique_id_name");
 				long uniqueIdValue = rs.getLong("unique_id_value");
-				_uniqueIdMap.put(uniqueIdName, new AtomicLong(uniqueIdValue));
+				if (uniqueIdName.length() == CP_APP_ID_LENGHT)
+				{
+					logger.info(uniqueIdName + ": " + uniqueIdValue);
+					_uniqueIdMap.put(uniqueIdName, new AtomicLong(uniqueIdValue));
+				}
 			}
 		}
 		catch (SQLException e)
 		{
 			e.printStackTrace();
 		}
+		
+		_future = Scheduler.getInstance().scheduleAtFixedRate(new PersistTask(), PERSIST_INTERVAL, PERSIST_INTERVAL, TimeUnit.MILLISECONDS);
 	}
 
 	@Override
@@ -84,6 +108,12 @@ public class UniqueIdDAOImpl implements UniqueIdDAO
 	@Override
 	public void destroy()
 	{
+		_future.cancel(false);
+		update();
+	}
+
+	private void update()
+	{
 		try
 		(
 			Connection conn = ConnectionFactory.getInstance().getConnection();
@@ -91,26 +121,38 @@ public class UniqueIdDAOImpl implements UniqueIdDAO
 			PreparedStatement updatePstmt = conn.prepareStatement("update unique_id set unique_id_value=? where unique_id_name=?");
 		)
 		{
+			_lock.lock();
+			
 			//insert
 			for (String newUniqueIdName : _newUniqueIdSet)
 			{
-				AtomicLong uniqueIdValue = _uniqueIdMap.remove(newUniqueIdName);
+				AtomicLong uniqueIdValue = _uniqueIdMap.get(newUniqueIdName);
 				pstmt.setString(1, newUniqueIdName);
 				pstmt.setLong(2, uniqueIdValue.longValue());
 				pstmt.executeUpdate();
+				logger.info(String.format("IAP insert (%s, %d)", newUniqueIdName, uniqueIdValue.get()));
 			}
 			//update
 			for (Map.Entry<String, AtomicLong> entry : _uniqueIdMap.entrySet())
 			{
+				if (_newUniqueIdSet.contains(entry.getKey()))
+				{
+					continue;
+				}
 				updatePstmt.setLong(1, entry.getValue().longValue());
 				updatePstmt.setString(2, entry.getKey());
 				updatePstmt.executeUpdate();
+				logger.info(String.format("IAP update (%s, %d)", entry.getKey(), entry.getValue()));
 			}
+			_newUniqueIdSet.clear();
 		}
 		catch (SQLException e)
 		{
 			e.printStackTrace();
 		}
+		finally
+		{
+			_lock.unlock();
+		}
 	}
-	
 }
